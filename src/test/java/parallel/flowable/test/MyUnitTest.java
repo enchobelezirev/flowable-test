7b8dcab058579c43c09c5ff53f0d0efa4069ba9a
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-07-06 16:48:06 +0300

    Fix test

--- src/test/java/parallel/flowable/test/MyUnitTest.java
+++ src/test/java/parallel/flowable/test/MyUnitTest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.ProcessDefinition;
+import org.flowable.engine.runtime.Execution;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.test.FlowableRule;
 import org.junit.Rule;
@@ -46,7 +47,25 @@ public class MyUnitTest {
                 .createProcessInstanceQuery()
                 .processInstanceId(procInst.getProcessInstanceId())
                 .singleResult();
-            System.out.println(processInstanceRunning.getActivityId());
+            List<Execution> executions = flowableRule.getRuntimeService().createExecutionQuery().list();
+            for (Execution execution : executions) {
+                System.out.println("--------------------------");
+                System.out.println("ID: " + execution.getId());
+                System.out.println("Activity ID: " + execution.getActivityId());
+                System.out.println("Name: " + execution.getName());
+                System.out.println("Is suspended: " + execution.isSuspended());
+                System.out.println("Is ended: " + execution.isEnded());
+                System.out.println("Super execution ID: " + execution.getSuperExecutionId());
+                System.out.println("Parent ID: " + execution.getParentId());
+                System.out.println("Process instance ID: " + execution.getProcessInstanceId());
+                System.out.println("--------------------------");
+                System.out.println();
+            }
+            System.out.println("==========================================================================");
+            System.out.println("==========================================================================");
+            System.out.println("==========================================================================");
+//            System.out.println(processInstanceRunning.getActivityId());
+            Thread.sleep(2000);
         }
 
     }
